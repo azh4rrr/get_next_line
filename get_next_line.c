2f3ba14f82d3df9bd4cde576b05bd17bbbbcf79c
Azhar Makhlouf <azmakhlo@c1r7p2.1337.ma>
2024-12-09 17:41:34 +0100

    get_next_line

---
+++ get_next_line.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azmakhlo <azmakhlo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/01 10:30:09 by azmakhlo          #+#    #+#             */
+/*   Updated: 2024/12/09 17:36:38 by azmakhlo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char	*ft_free(char **str)
+{
+	if (str && *str)
+	{
+		free(*str);
+		*str = NULL;
+	}
+	return (NULL);
+}
+
+static void	ft_cpy(char **dst, char **src)
+{
+	int	i;
+
+	i = 0;
+	if (*dst && *src)
+	{
+		while ((*src)[i] && (*src)[i] != '\n')
+		{
+			(*dst)[i] = (*src)[i];
+			i++;
+		}
+		if ((*src)[i] == '\n')
+			(*dst)[i++] = '\n';
+		(*dst)[i] = '\0';
+	}
+}
+
+static char	*extra_line(char **line)
+{
+	int		i;
+	char	*dst;
+	char	*temp;
+
+	i = 0;
+	while ((*line)[i] && (*line)[i] != '\n')
+		i++;
+	if ((*line)[i] == '\n')
+		i++;
+	dst = malloc(i + 1);
+	if (!dst)
+		return (ft_free(line));
+	ft_cpy(&dst, line);
+	temp = ft_strdup((*line) + i);
+	if (!temp)
+		return (ft_free(line), ft_free(&dst));
+	ft_free(line);
+	*line = temp;
+	return (dst);
+}
+
+static char	*ft_get_line(int fd, char **line)
+{
+	char	*buffer;
+	int		nb;
+
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (ft_free(line));
+	while (1)
+	{
+		nb = read(fd, buffer, BUFFER_SIZE);
+		if (nb < 0 && (!*line || !**line))
+			return (ft_free(line), ft_free(&buffer));
+		buffer[nb] = '\0';
+		*line = ft_strjoin(*line, buffer);
+		if (!*line || !line)
+			return (ft_free(line), ft_free(&buffer));
+		if (ft_strchr(*line, '\n') || nb == 0)
+			break ;
+	}
+	ft_free(&buffer);
+	return (*line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*line;
+	char		*extra;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, NULL, 0) < 0)
+		return (ft_free(&line));
+	line = ft_get_line(fd, &line);
+	if (!line)
+		return (ft_free(&extra));
+	extra = extra_line(&line);
+	if (!extra || ft_strlen(extra) == 0)
+		return (ft_free(&extra), ft_free(&line));
+	return (extra);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azmakhlo <azmakhlo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/09 17:10:27 by azmakhlo          #+#    #+#             */
+/*   Updated: 2024/12/09 17:10:30 by azmakhlo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <fcntl.h>
+# include <limits.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 100
+# endif
+
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strdup(const char *s);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*get_next_line(int fd);
+
+#endif
